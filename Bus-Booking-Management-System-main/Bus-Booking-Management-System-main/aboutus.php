a5477333b36c0d02fc2ed143723fa1ee828cb7bb
adityaraj9900 <102577534+adityaraj9900@users.noreply.github.com>
2023-09-11 23:07:44 +0530

    Add files via upload

---
+++ Bus-Booking-Management-System-main/Bus-Booking-Management-System-main/aboutus.php
@@ -0,0 +1,50 @@
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>About Us</title>
+
+    <link rel="stylesheet" type="text/css" href="css/aboutus.css">
+    <link rel="stylesheet" type="text/css" href="css/NNP_stylesheet.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+</head>
+
+<body>
+    <header>
+        <div class="title">
+            <h1>Easy Journey</h1>
+        </div>
+        <div class=main>
+            <a href="0index.php"><i class="fas fa-home"></i> Home</a>
+
+
+            <div class="box">
+
+
+              
+
+
+
+                <p>“Travel is the main thing you purchase that makes you more extravagant”. We, at ‘Organization Name’,
+                    swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly.
+                    We have been moving excellent encounters for a considerable length of time through our cutting edge
+                    planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry
+                    on with a rich life, brimming with extraordinary travel encounters.
+
+                    Through our exceptionally curated occasion bundles, we need to take you on an adventure where you
+                    personally enjoy with the stunning magnificence of America and far off terrains. We need you to
+                    observe sensational scenes that are a long ways past your creative ability.
+
+                    The powerful inclination of American voyagers to travel more nowadays is something that keeps us
+                    inspired to satisfy your vacation necessities. Our vision to give you a consistent occasion
+                    encounter makes us one of the main visit administrators in the regularly extending travel industry.
+                </p>
+            </div>
+
+
+    </header>
+    <hr>
+    
+</body>
+
+</html>
